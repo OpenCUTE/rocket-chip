8c2b4ad9a56192b0e9a600b2fc14fc13a743e71b
Jinpeng Ye <yejinpeng21s@ict.ac.cn>
2026-05-11 23:54:56 +0800

    [update]update add remote_bitbang debug

--- src/main/resources/csrc/remote_bitbang.cc
+++ src/main/resources/csrc/remote_bitbang.cc
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 
 #include <algorithm>
 #include <cassert>
@@ -21,7 +22,9 @@ remote_bitbang_t::remote_bitbang_t(uint16_t port) :
   client_fd(0),
   recv_start(0),
   recv_end(0),
-  err(0)
+  err(0),
+  execute_count(0),
+  timing_log(NULL)
 {
   socket_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (socket_fd == -1) {
@@ -39,11 +42,26 @@ remote_bitbang_t::remote_bitbang_t(uint16_t port) :
     abort();
   }
 
+  // Check for environment variable to override port
+  uint16_t actual_port = port;
+  if (port == 0) {
+    const char* port_env = getenv("REMOTE_BITBANG_PORT");
+    if (port_env != NULL) {
+      int env_port = atoi(port_env);
+      if (env_port > 0 && env_port <= 65535) {
+        actual_port = (uint16_t)env_port;
+        fprintf(stderr, "Using port from REMOTE_BITBANG_PORT: %d\n", actual_port);
+      } else {
+        fprintf(stderr, "Invalid REMOTE_BITBANG_PORT value '%s', using auto-assigned port\n", port_env);
+      }
+    }
+  }
+
   struct sockaddr_in addr;
   memset(&addr, 0, sizeof(addr));
   addr.sin_family = AF_INET;
   addr.sin_addr.s_addr = INADDR_ANY;
-  addr.sin_port = htons(port);
+  addr.sin_port = htons(actual_port);
 
   if (::bind(socket_fd, (struct sockaddr *) &addr, sizeof(addr)) == -1) {
     fprintf(stderr, "remote_bitbang failed to bind socket: %s (%d)\n",
@@ -69,10 +87,48 @@ remote_bitbang_t::remote_bitbang_t(uint16_t port) :
   tdi = 1;
   trstn = 1;
   quit = 0;
+  execute_count = 0;
+
+  // Check for environment variable to override log file path
+  const char* log_path = getenv("REMOTE_BITBANG_LOG");
+  if (log_path == NULL) {
+    log_path = "remote_bitbang_timing.log";
+  }
+
+  // Open timing log file
+  timing_log = fopen(log_path, "w");
+  if (timing_log) {
+    fprintf(timing_log, "remote_bitbang execution timing log\n");
+    fprintf(timing_log, "Format: execute_count,command,read_ns,process_ns,total_ns\n");
+    fprintf(timing_log, "Log file: %s\n", log_path);
+    fflush(timing_log);
+    fprintf(stderr, "Remote bitbang timing log: %s\n", log_path);
+  } else {
+    fprintf(stderr, "Warning: Failed to open timing log file '%s'\n", log_path);
+  }
 
   fprintf(stderr, "This emulator compiled with JTAG Remote Bitbang client. To enable, use +jtag_rbb_enable=1.\n");
   fprintf(stderr, "Listening on port %d\n",
          ntohs(addr.sin_port));
+}
+
+remote_bitbang_t::~remote_bitbang_t()
+{
+  // Close timing log file
+  if (timing_log) {
+    fprintf(timing_log, "\n=== Log closed ===\n");
+    fprintf(timing_log, "Total commands executed: %lu\n", execute_count);
+    fclose(timing_log);
+    timing_log = NULL;
+  }
+
+  // Close socket
+  if (socket_fd > 0) {
+    close(socket_fd);
+  }
+  if (client_fd > 0) {
+    close(client_fd);
+  }
 }
 
 void remote_bitbang_t::accept()
@@ -133,6 +189,9 @@ void remote_bitbang_t::set_pins(char _tck, char _tms, char _tdi){
 
 void remote_bitbang_t::execute_command()
 {
+  struct timespec start_read, end_read, end_process;
+  clock_gettime(CLOCK_MONOTONIC, &start_read);
+
   char command;
   int again = 1;
   while (again) {
@@ -154,6 +213,8 @@ void remote_bitbang_t::execute_command()
       again = 0;
     }
   }
+
+  clock_gettime(CLOCK_MONOTONIC, &end_read);
 
   //fprintf(stderr, "Received a command %c\n", command);
 
@@ -198,5 +259,20 @@ void remote_bitbang_t::execute_command()
     fprintf(stderr, "Remote end disconnected\n");
     close(client_fd);
     client_fd = 0;
+  }
+
+  clock_gettime(CLOCK_MONOTONIC, &end_process);
+
+  long read_ns = (end_read.tv_sec - start_read.tv_sec) * 1000000000L +
+                 (end_read.tv_nsec - start_read.tv_nsec);
+  long process_ns = (end_process.tv_sec - end_read.tv_sec) * 1000000000L +
+                    (end_process.tv_nsec - end_read.tv_nsec);
+  long total_ns = (end_process.tv_sec - start_read.tv_sec) * 1000000000L +
+                  (end_process.tv_nsec - start_read.tv_nsec);
+
+  if (timing_log) {
+    execute_count++;
+    fprintf(timing_log, "%lu,%c,%ld,%ld,%ld\n", execute_count, command, read_ns, process_ns, total_ns);
+    fflush(timing_log);
   }
 }
